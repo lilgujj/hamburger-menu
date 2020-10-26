9d7eab273650e66c089ff9995efc55e5a05b4fcf
lilgujj <gustavkarlandersson@hotmail.com>
2020-10-26 14:21:22 +0100

    hamburger toggle on off

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+let HamburgerMenuStatus = false;
 
 
-// function for onlick header height 100%.
-// function for display: unset; for header span.
+function hamburgerMenuToggle() {
+    let getHamburgerMenu = document.querySelector("header");
+    let spanVisible = document.querySelector(".navbar");
+
+    if (HamburgerMenuStatus === false) {
+        getHamburgerMenu.style.height = "100%";
+        spanVisible.style.display = "unset";
+
+        HamburgerMenuStatus = true;
+    }
+
+
+    else if (HamburgerMenuStatus === true) {
+        getHamburgerMenu.style.height = "4rem";
+        spanVisible.style.display = "none";
+
+        HamburgerMenuStatus = false;
+    }
+
+
+}
+
+
 
 // function for animation to fade in, prob in css.
